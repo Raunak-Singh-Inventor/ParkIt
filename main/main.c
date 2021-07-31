b22a6066dde1c2742112768497f16c07917bdd72
Raunak-Singh-Inventor <raunak.singh.inventor@gmail.com>
2021-07-31 08:08:26 -0400

    added static chart examppel to gsr page

--- main/main.c
+++ main/main.c
@@ -439,6 +439,44 @@ static void sensor_btnm_event_handler(lv_obj_t * obj, lv_event_t event)
         printf("%s was pressed\n", txt);
         if(strcmp(txt,"GSR")==0) {
             // gsr button was pressed
+            /*********/
+            /* Chart */
+            lv_obj_t * chart;
+            chart = lv_chart_create(lv_scr_act(), NULL);
+            lv_obj_set_size(chart, 200, 150);
+            lv_obj_align(chart, NULL, LV_ALIGN_CENTER, 0, 10);
+            lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
+
+            /*Add two data series*/
+            lv_chart_series_t * ser1 = lv_chart_add_series(chart, LV_COLOR_RED);
+            lv_chart_series_t * ser2 = lv_chart_add_series(chart, LV_COLOR_GREEN);
+
+            /*Set the next points on 'ser1'*/
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 10);
+            lv_chart_set_next(chart, ser1, 30);
+            lv_chart_set_next(chart, ser1, 70);
+            lv_chart_set_next(chart, ser1, 90);
+
+            /*Directly set points on 'ser2'*/
+            ser2->points[0] = 90;
+            ser2->points[1] = 70;
+            ser2->points[2] = 65;
+            ser2->points[3] = 65;
+            ser2->points[4] = 65;
+            ser2->points[5] = 65;
+            ser2->points[6] = 65;
+            ser2->points[7] = 65;
+            ser2->points[8] = 65;
+            ser2->points[9] = 65;
+
+            lv_chart_refresh(chart);
+            /*********/
         } else if(strcmp(txt,"Mic")==0) {
             // mic button was pressed
         } else {
